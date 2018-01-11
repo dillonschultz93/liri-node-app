d77209f8f89e4100d772c9a5c1dcb4c8c436a592
Dillon Schultz <dillonschultz93@gmail.com>
2018-01-10 21:28:18 -0700

    Initial commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('keys.js is loaded')
+
+const twitterKeys = {
+  consumer_key: 'ATBq5hKSo2c5qYTWtz8B0whtU',
+  consumer_secret: 'kPnVF14nQCXNTz0qfT6nwsJeeymaeJdGCl6joKj7rf6ECKvWEr',
+  access_token_key: '403945444-JW27KeakRyV1Z2jwgRtNSMyvSfGh19VZKeR8ejx0',
+  access_token_secret: 'e5Qrs0hggUowPIalGScBUUtadAh69ezUXnyypiBKqtQhZ'
+}
+
+module.export = twitterKeys
