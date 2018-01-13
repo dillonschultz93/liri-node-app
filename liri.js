3906654b94380b9d64fbffaf20e570c062d5fc6e
Dillon Schultz <dillonschultz93@gmail.com>
2018-01-13 16:23:48 -0700

    Modularized the codebase. Now requires 'commands.js'

---
+++ liri.js
@@ -0,0 +1,21 @@
+// imports
+const nodeCommands = require('./commands.js')
+const fileSystem = require('fs')
+
+
+// input variables
+const command = process.argv[2]
+const value = process.argv[3]
+
+//clear console everytime the file is run
+process.stdout.write('\x1B[2J\x1B[3J\x1B[H')
+
+switch(command){
+  case 'my-tweets':
+    nodeCommands.twitterCommand()
+  break;
+  default:
+    console.log('---------------------------------')
+    console.log('Sorry, that command is not valid.')
+    console.log('---------------------------------')
+}
